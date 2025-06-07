b5b2d0a1ece2878f90176d6e3ac641964ba43b15
toosolid2003 <toosolid2003@yahoo.fr>
2025-06-07 17:10:57 +0200

    Initial commit

---
+++ src/main.jsx
@@ -0,0 +1,9 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import AppWrapper from './AppWrapper';
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <AppWrapper />
+  </StrictMode>,
+)
