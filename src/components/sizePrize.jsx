ec0bb3c13f3f12bd129027254e3fc9d843d17964
toosolid2003 <toosolid2003@yahoo.fr>
2025-06-12 13:14:01 +0200

    Fixed the fetchBalance function - was calling the wrong contract function

--- src/components/sizePrize.jsx
+++ src/components/sizePrize.jsx
@@ -2,38 +2,44 @@ import { useState, useEffect } from 'react'
 import { ethers } from 'ethers';
 
 
-// Set useState 
-
-
-
 // Retrieve the contract balance
 function GetBalance({ contract })   {
     const [balance, setBalance] = useState(null);
     
     useEffect(()    => {
+        if(!contract) return;
+        
+        let isMounted = true;
+        
         async function fetchBalance()   {
-            if(!contract) return;
+            try{
+                const b = await contract.checkContractBalance();
+                if(isMounted)   {
+                    setBalance(ethers.formatEther(b));
+                }
+            }   
+            catch (err)  {
+                    console.error("Error fetching balance", err);
+                }
+        };
 
-            const b = await contract.checkBalance();
-            setBalance(ethers.formatEther(b));
-    
-        }
-
-    fetchBalance(); // calling the function just after being defined
-    
     const onDeposit = (sender, amount) => {
         console.log("New deposit from:", sender, "for ", amount.toString());
         fetchBalance(); // Fetchbalance everytime a new deposit is made
     };
 
+    fetchBalance();
     contract.on('Deposit', onDeposit);
 
     return() => {
+        isMounted = false;
         contract.off('Deposit', onDeposit);
     };
 
     }, [contract]   // Also, everytime the contract variable changes, call the fecthBalance function
     );
+
+    if(!contract) return null;
 
     return(
         <div>
@@ -45,5 +51,3 @@ function GetBalance({ contract })   {
 }
 
 export default GetBalance;
-
-// event Deposit(address indexed contributor, uint amount);
